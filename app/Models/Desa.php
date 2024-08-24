2957728ffc1b51539a9499f5f1b658aeaefabc6b
kevin hendra <vient74@gmail.com>
2024-08-24 09:06:16 +0700

    update desa

--- app/Models/Desa.php
+++ app/Models/Desa.php
@@ -58,6 +58,71 @@ class Desa extends Model
 
     protected function indexDesa($max_data)
     {
+       $query = DB::table('master.master_desa')
+            ->select(
+                'master.master_desa.id',
+                'master.master_desa.name',
+                'master.master_desa.long_name',
+                'master.master_desa.updated_at',
+                DB::raw('COUNT(DISTINCT master.master_kpm.id) AS jumlah_kpm'),
+                DB::raw('COUNT(DISTINCT master.master_user.id) AS jumlah_user'),
+                DB::raw('COUNT(DISTINCT master.master_meta_kk.kk) AS jumlah_kk'),
+                DB::raw('COUNT(DISTINCT master.master_meta_sasaran.nik) AS jumlah_sasaran')
+            )
+            ->leftJoin('master.master_meta_kk', 'master.master_desa.id', '=', 'master.master_meta_kk.desa_id')
+            ->leftJoin('master.master_meta_sasaran', 'master.master_desa.id', '=', 'master.master_meta_sasaran.desa_id')
+            ->leftJoin('master.master_user', 'master.master_desa.id', '=', 'master.master_user.desa_id')
+            ->leftJoin('master.master_kpm', 'master.master_desa.id', '=', 'master.master_kpm.desa_id')
+            ->groupBy('master.master_desa.id', 'master.master_desa.name', 'master.master_desa.long_name');
+
+        if (Auth::user()->role->tag == 'admin_prov') {
+            $query->where(DB::raw('LEFT(master.master_desa.id, 2)'), '=', Auth::user()->provinsi_id);
+        }
+
+        $desas = $query->orderBy('id', 'ASC')->cursorPaginate($max_data);
+
+        return $desas;             
+
+    }
+
+    protected function indexDesaSearch($query, $max_data)
+    {
+        $sql = DB::table(DB::raw('(
+                    SELECT 
+                           master.master_desa.id,
+                           master.master_desa.name,
+                           master.master_desa.long_name,
+                           master.master_desa.updated_at,
+                           COUNT(DISTINCT master.master_kpm.id)     AS jumlah_kpm,
+                           COUNT(DISTINCT master.master_user.id)    AS jumlah_user,
+                           COUNT(DISTINCT master.master_meta_kk.kk) AS jumlah_kk,
+                           COUNT(DISTINCT master.master_meta_sasaran.nik) AS jumlah_sasaran
+                      FROM master.master_desa
+                 LEFT JOIN master.master_meta_kk      ON master.master_desa.id = master.master_meta_kk.desa_id
+                 LEFT JOIN master.master_meta_sasaran ON master.master_desa.id = master.master_meta_sasaran.desa_id
+                 LEFT JOIN master.master_user ON master.master_user.desa_id = master.master_desa.id
+                 LEFT JOIN master.master_kpm  ON master.master_kpm.desa_id  = master.master_desa.id
+                  GROUP BY master.master_desa.id, master.master_desa.name, master.master_desa.long_name
+                ) AS subquery'));
+
+                if (Auth::user()->role->tag == 'admin_prov') {
+                   // $sql->where(DB::raw("left(subquery.id, 2)"), '=', Auth::user()->provinsi_id);
+                   // $sql->where(DB::raw("subquery.id"), 'like', Auth::user()->provinsi_id . '%');
+                   $sql->where(DB::raw('substring(subquery.id, 1, 2)'), '=', Auth::user()->provinsi_id);
+                }
+
+                $sql->where("subquery.name", 'like', '%' . $query . '%')
+                    ->orWhere('long_name', 'like', '%' . $query . '%')
+                    ->orWhere('subquery.id', $query);
+
+                $desas = $sql->orderBy('id', 'ASC')
+                             ->cursorPaginate($max_data);
+        return $desas;
+    }
+
+
+    protected function monitoringDesa($max_data)
+    {
         $sql = DB::table(DB::raw('(
                     SELECT 
                            master.master_desa.id,
@@ -85,7 +150,7 @@ class Desa extends Model
         return $desas;
     }
 
-    protected function indexDesaSearch($query, $max_data)
+    protected function monitoringDesaSearch($query, $max_data)
     {
         $sql = DB::table(DB::raw('(
                     SELECT 
